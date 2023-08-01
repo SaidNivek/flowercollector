3ee7013fa6ef25d5552f0f46903edd189a34b7cf
SaidNivek <kevin.vi.dias@gmail.com>
2023-07-31 21:15:42 -0400

    Add Flowers index and render some dummy data from views.py

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('flowers/', views.flowers_index, name='about'),
 ]
