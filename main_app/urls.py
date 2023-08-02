926bda2aa7e6725151f1cbbddabb51acb239dbdd
SaidNivek <kevin.vi.dias@gmail.com>
2023-08-02 17:04:29 -0400

    Update a tags in base.html to use django syntax

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('flowers/', views.flowers_index, name='about'),
+    path('flowers/', views.flowers_index, name='index'),
     path('flowers/<int:flower_id>/', views.flowers_detail, name='detail')
 ]
