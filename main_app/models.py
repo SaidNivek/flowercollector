3e932a436694907e11470811ac73ee9625ac9f10
SaidNivek <kevin.vi.dias@gmail.com>
2023-08-01 23:04:10 -0400

    Add Flower model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Flower(models.Model):
+    name: models.CharField(max_length=100)
+    plantType: models.CharField(max_length=100)
+    bloom: models.TextField(max_length=250)
+    height: models.IntegerField()
+    spacing: models.IntegerField()
+    pinch: models.CharField(max_length=100)   
+    deerResistant: models.BooleanField()
+    image: models.CharField(max_length=100)
