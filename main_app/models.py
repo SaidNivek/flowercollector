ea74aa2f755d081cc1524dd8ec720322a24af69f
SaidNivek <kevin.vi.dias@gmail.com>
2023-08-02 00:07:03 -0400

    Add Flower model to models.py, override __str__ method

--- main_app/models.py
+++ main_app/models.py
@@ -2,11 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Flower(models.Model):
-    name: models.CharField(max_length=100)
-    plantType: models.CharField(max_length=100)
-    bloom: models.TextField(max_length=250)
-    height: models.IntegerField()
-    spacing: models.IntegerField()
-    pinch: models.CharField(max_length=100)   
-    deerResistant: models.BooleanField()
-    image: models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    plantType = models.CharField(max_length=100)
+    bloom = models.TextField(max_length=250)
+    height = models.CharField(max_length=100)
+    spacing = models.CharField(max_length=100)
+    hardiness = models.CharField(max_length=100)
+    pinch = models.CharField(max_length=100)   
+    deerResistant = models.BooleanField()
+    image = models.TextField(max_length=1000)
+
+    def __str__(self):
+        return f"This flower is called {self.name} and is in the family of {self.plantType}."
